f7bc9b080d98cc6f1df876d70f32c9a1a5a145f0
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-05-13 22:30:58 +0200

    Fixed the vue actions

--- server/src/main/java/com/project/server/component/Seeder.java
+++ server/src/main/java/com/project/server/component/Seeder.java
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile({"local", "dev"})
+@Profile({"local", "test"})
 public class Seeder implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
